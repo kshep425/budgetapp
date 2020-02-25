9dc7098dea24096178d1172d3fc3c4b21d4a2aed
Keisha <kshep425@gmail.com>
2020-02-24 22:01:04 -0500

    Starting the beginning of the budget app

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compress = require("compression");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const DB = process.env.DB || "mongodb://localhost/budget"
+
+dbOptions = {
+    useNewUrlParser: true,
+    useFindAndModify: false
+}
+mongoose.connect(DB, dbOptions);
+
+//routes
+
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`)
+})
