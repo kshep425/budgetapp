1f40ab0070f6e2922e72cfb44703bbd07fc26856
Keisha <kshep425@gmail.com>
2020-02-26 21:30:17 -0500

    Added form elements to table

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,5 +1,21 @@
-document.ready(()=>{
-    document.getElementById("form_submit").click(()=>{
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Document Ready")
+    var form_description = document.getElementById("form_description")
+    var form_cost = document.getElementById("form_cost")
+    document.getElementById("form_submit").addEventListener("click", (event)=>{
+        event.preventDefault()
         console.log("Form submitted")
+        add_to_table(form_description.value, form_cost.value)
+
     })
+
+    function add_to_table(description, cost){
+        console.log(`add ${description} and ${cost} to table`)
+        var table_row = document.createElement("tr")
+        var col_description = document.createElement("td").textContent = description
+        var col_cost = document.createElement("td").textContent = cost
+
+        table_row.append(col_description, col_cost)
+        document.getElementById("table_expenses").append(table_row)
+    }
 })
