91a5b657ac445223b335c8d7abcb13c44737e7d6
Keisha <kshep425@gmail.com>
2020-03-14 11:17:45 -0400

    Working with Es-lint

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,47 +1,48 @@
-const db_queries = require("../db/db_queries")
+const db_queries = require("../db/db_queries");
 const api_routes = function (app) {
     app.get("/api/expenses", function (req, res) {
-        console.log(req.body)
+        // console.log(req.body);
         db_queries.get_expenses()
             .then(result => {
-                console.log("++++++++++++++Get Expenses+++++++++++++")
-                console.log(result)
-                res.json(result)
+                // console.log("++++++++++++++Get Expenses+++++++++++++");
+                // console.log(result);
+                res.json(result);
             })
             .catch((err) => {
-                console.log(err)
-                res.status(400).json(err)
-            })
-    })
+                console.log(err);
+                res.status(400).json(err);
+            });
+    });
 
     app.post("/api/add_expense", (req, res) => {
-        console.log(req.body)
+        console.log("add expense");
+        console.log(req.body);
         db_queries.add_expense(req.body.description, req.body.cost)
             .then(result => {
-                console.log("++++++++++++++Add Expense+++++++++++++")
-                console.log(result)
+                console.log("++++++++++++++Add Expense+++++++++++++");
+                console.log(result);
 
-                res.send(result)
+                res.send(result);
             })
             .catch((err) => {
-                console.log("--------------Error---------------")
-                console.log(err.errors)
-                res.status(400).json(err.errors)
-            })
-    })
+                console.log("--------------Error---------------");
+                console.log(err.errors);
+                res.status(400).json(err.errors);
+            });
+    });
 
     app.post("/api/transaction/bulk", (req, res) =>{
         db_queries.bulk_add(req.body)
             .then(result =>{
-                console.log("_____________Bulk Add_________________")
-                console.log(result)
-                res.json(result)
+                //console.log("_____________Bulk Add_________________");
+                //console.log(result);
+                res.json(result);
             })
             .catch((err) => {
-                console.log(err.errors)
-                res.status(400).json(err.errors)
-            })
-    })
-}
+                console.log(err.errors);
+                res.status(400).json(err.errors);
+            });
+    });
+};
 
-module.exports = api_routes
+module.exports = api_routes;
